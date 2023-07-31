5befc30346ba391728dddbf4be8a537d1a778dd4
Sreeja <sreejamadireddy1998@gmail.com>
2023-07-31 20:37:08 +0530

    Get All Jobs and Get single Job

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -2,12 +2,22 @@ const Job = require("../models/Job");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 const getJob = async (req, res) => {
-  res.send("get job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findOne({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError(`No Job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 const createJob = async (req, res) => {
+  console.log(req.user);
   req.body.createdBy = req.user.userId;
   // console.log(`req.body.createdBy ${req.body.createdBy}`);
   // console.log(`req.user.userId ${req.user.userId}`);
