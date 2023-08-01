21def3d77c68a12ab3724e221f7f565cad439a50
Sreeja <sreejamadireddy1998@gmail.com>
2023-08-01 14:21:00 +0530

    setting up security

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
+
+//security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -14,6 +21,16 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMS: 15 * 60 * 1000, //15 minutes
+    max: 100, // limit each IP to 100 requests per windows
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 app.use(express.json());
 
 //routes
